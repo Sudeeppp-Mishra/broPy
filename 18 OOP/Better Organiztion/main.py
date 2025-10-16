d17cb3b6ce8b1782c6d38c1a3ebe8d3069a4fd1c
mishrasm2022@gmail.com <239933501+Sudeeppp-Mishra@users.noreply.github.com>
2025-10-16 12:31:50 +0545

    feat: intro to OOP added

---
+++ 18 OOP/Better Organiztion/main.py
@@ -0,0 +1,17 @@
+from car import Car
+
+car1 = Car("Mustang", 2025, "red", False)
+
+print(car1.model)
+print(car1.year)
+print(car1.color)
+print(car1.for_sale)
+        
+        
+print()
+
+car2 = Car("Corvette", 2026, "blue", True)
+
+print(car2.model)
+print(car2.year)
+print(car2.color)
